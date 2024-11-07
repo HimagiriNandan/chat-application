f69b34076ed331ef4ff4197900bf0defde4a6ad9
Himagiri Nandan <himagirinandantuduru@gmail.com>
2024-11-07 21:59:51 +0530

    updated frontend url

--- client/my-project/src/pages/chat/components/contacts-container/index.jsx
+++ client/my-project/src/pages/chat/components/contacts-container/index.jsx
@@ -85,7 +85,7 @@ const Logo = () => {
           fill="#a16ee8"
         ></path>{" "}
       </svg>
-      <span className="text-3xl font-semibold ">Syncronus</span>
+      <span className="text-3xl font-semibold ">SyncWave</span>
     </div>
   );
 };
