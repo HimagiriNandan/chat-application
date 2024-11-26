6f32e68e0e066e3a1ee7a3e3c450042178b0cc26
Himagiri Nandan <himagirinandantuduru@gmail.com>
2024-11-26 22:07:49 +0530

    wikipedia button updated

--- client/my-project/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/my-project/src/pages/chat/components/empty-chat-container/index.jsx
@@ -1,8 +1,10 @@
 import { animationDefaultOptions } from "@/lib/utils";
 import Lottie from "react-lottie";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const EmptyContainer = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all">
       <Lottie 
@@ -11,7 +13,12 @@ const EmptyContainer = () => {
       width={200}
       options={animationDefaultOptions}
       />
-      <div className="w-full">Search here</div>
+      <Button
+        className="mt-6 px-6 py-2 text-white bg-[#3a3b43] rounded-full hover:bg-[#1c1d25] focus:outline-none"
+        onClick={() => navigate("/search")}
+      >
+        Wiki Search
+      </Button>
       <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center">
         <h3 className="poppins-medium">
           Hi<span className="text-purple-500">! </span>Welcome to <span className="text-purple-500">Syncronus </span>Chat App <span className="text-purple-500">.</span>
